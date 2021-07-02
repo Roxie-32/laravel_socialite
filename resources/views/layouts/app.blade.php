d19d9b6b2aec1160f9214583877ff67b2b55d342
Roxie-32 <fizzyflexy397@gmail.com>
2021-07-02 09:29:26 +0100

    UI minor changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,24 +52,24 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <img src="{{Auth::user()->avatar}}" alt="{{Auth::user()->name}}" style="border:1px solid #cccccc; width:39px; float:left; height: auto; margin-right:7px;">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                         
+<li class="nav-item dropdown">
+    <img src="{{Auth::user()->avatar}}" alt="{{Auth::user()->name}}" style="border:1px solid #cccccc; width:39px; float:left; height: auto; margin-right:7px;">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+       </a>
+          <div class="dropdown-menu dropdown-menu-right" 
+            aria-labelledby="navbarDropdown">
+       <a class="dropdown-item" href="{{ route('logout') }}" 
+          onclick="event.preventDefault();  
+            document.getElementById('logout-form').submit();">
+                 {{ __('Logout') }}
+           </a>
+<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+</form>
+     </div>
+ </li>
                         @endguest
                     </ul>
                 </div>
